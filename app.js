bd6a01d295c27857639e3d860e89fdfa7670b3e4
Kareem <kemo.friends@gmail.com>
2026-05-04 01:20:46 +0300

    fix: project name

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Keka is Running away");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my subscribtion tracker api');
+});
+
+app.listen(3000, ()=> {
+  console.log('server is running on port : 3000 on http://localhost:3000');
+});
+
+export default app;
